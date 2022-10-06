979a1ab7cc6392e9af97fffb4a9ae381a1a89bdc
Mostafa Aboelnaga <66335224+mostafa-aboelnaga@users.noreply.github.com>
2022-10-06 08:45:45 +0200

    updated fetching URLs

--- utils/fetchExperiences.ts
+++ utils/fetchExperiences.ts
@@ -2,7 +2,7 @@ import { Experience } from "../typings";
 
 export const fetchExperiences = async () => {
   const res = await fetch(
-    `/api/getExperience`,
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperience`,
   );
   const data = await res.json();
   const experiences: Experience[] = data.experiences;
--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -2,7 +2,7 @@ import { PageInfo } from "../typings";
 
 export const fetchPageInfo = async () => {
   const res = await fetch(
-    `/api/getPageInfo`,
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`,
   );
   const data = await res.json();
   const pageInfo: PageInfo = data.pageInfo;
